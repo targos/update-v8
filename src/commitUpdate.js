aa8def36ab9c4e27fca92b3caf2ad3564a2b8990
Michaël Zasso <targos@protonmail.com>
2017-11-11 16:32:58 +0100

    use a different commit message for minor updates
    Closes: #11

--- src/commitUpdate.js
+++ src/commitUpdate.js
@@ -11,12 +11,16 @@ module.exports = function () {
             const newV8Version = util.getNodeV8Version(ctx.nodeDir).join('.');
             await ctx.execGitNode('add', 'deps/v8');
             const moreArgs = [];
+            let message;
             if (ctx.minor) {
                 const prev = ctx.currentVersion.join('.');
                 const next = ctx.latestVersion.join('.');
                 moreArgs.push('-m', `Refs: https://github.com/v8/v8/compare/${prev}...${next}`);
+                message = `deps: patch V8 to ${newV8Version}`;
+            } else {
+                message = `deps: update V8 to ${newV8Version}`;
             }
-            await ctx.execGitNode('commit', '-m', `deps: update V8 to ${newV8Version}`, ...moreArgs);
+            await ctx.execGitNode('commit', '-m', message, ...moreArgs);
         },
         skip: (ctx) => ctx.skipped
     };
